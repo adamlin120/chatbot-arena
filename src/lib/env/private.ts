e4c4c4afc08da05445d28b6521bb1af25e041f82
quevenliu <108058646+quevenliu@users.noreply.github.com>
2024-03-27 21:56:34 +0800

    Update to new version

--- src/lib/env/private.ts
+++ src/lib/env/private.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
 const privateEnvSchema = z.object({
-  POSTGRES_URL: z.string().url(),
+  DATABASE_URL: z.string(),
   OPENAI_KEY: z.string(),
 });
 
 type PrivateEnv = z.infer<typeof privateEnvSchema>;
 
 export const privateEnv: PrivateEnv = {
-  POSTGRES_URL: process.env.POSTGRES_URL!,
+  DATABASE_URL: process.env.DATABASE_URL!,
   OPENAI_KEY: process.env.OPENAI_KEY!,
 };
 
