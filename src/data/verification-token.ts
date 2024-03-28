0b78d772431c5434200e3ce66e571156584388bf
quevenliu <108058646+quevenliu@users.noreply.github.com>
2024-03-28 17:30:57 +0800

    refactor(add commitizen): add Commitizen Init to the Repo

--- src/data/verification-token.ts
+++ src/data/verification-token.ts
@@ -2,64 +2,6 @@ import { PrismaClient } from '@prisma/client';
 
 const db = new PrismaClient();
 
-/* schema
-
-// schema.prisma
-
-datasource db {
-  provider = "mongodb"
-  url      = env("DATABASE_URL")
-}
-
-generator client {
-  provider = "prisma-client-js"
-}
-
-model User {
-  id            String   @id @default(auto()) @map("_id") @db.ObjectId
-  email         String   @unique
-  username      String
-  provider      String
-  hashedPassword String
-  coins         Int
-  avatarUrl     String
-  bio           String
-  verified      Boolean
-  conversations Conversation[]
-}
-
-model Verification {
-  email         String    @id @map("_id")
-  token         String         
-  expires       DateTime 
-}
-
-model Conversation {
-  id             String   @id @map("_id") @default(auto()) @db.ObjectId
-  contributor    User     @relation(fields: [contributorId], references: [id])
-  contributorId  String   @db.ObjectId
-  details  ConversationDetail[]
-}
-
-model ConversationDetail {
-  id         String   @id @map("_id") @default(auto()) @db.ObjectId
-  prompt     String
-  completions Completion[]
-  feedback   String?
-  conversation Conversation @relation(fields: [conversationId], references: [id])
-  conversationId String @db.ObjectId
-}
-
-model Completion {
-  id         String   @id @map("_id") @default(auto()) @db.ObjectId
-  content    String
-  model_name String
-  rating     Int
-  conversationDetail ConversationDetail @relation(fields: [conversationDetailId], references: [id])
-  conversationDetailId String @db.ObjectId
-}
-
-*/
 export const getVerificationTokenByToken = async (token: string) => {
     try {
         const vtoken = await db.verification.findFirst({
