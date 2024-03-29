f398ddee26c7fb18e1d7c8aebb82bde58960a2fc
quevenliu <108058646+quevenliu@users.noreply.github.com>
2024-03-29 17:47:19 +0800

    feat(stream): randomly choose model for chat stream
    The authorization of Anthropic and Mistral model is currently unavailable, future checks need to be
    make

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,39 +1,84 @@
 import { Message } from "@/lib/types/db";
 import { NextRequest, NextResponse } from "next/server";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { PrismaClient } from "@prisma/client";
+import { StreamingTextResponse } from "ai";
+import getStream from "@/lib/chat/stream";
+/* Database Schema
+// schema.prisma
 
-import OpenAI from "openai";
-import { privateEnv } from "@/lib/env/private";
+datasource db {
+  provider = "mongodb"
+  url      = env("DATABASE_URL")
+}
 
-const openai = new OpenAI({
-  apiKey: privateEnv.OPENAI_KEY, 
-});
+generator client {
+  provider = "prisma-client-js"
+}
+
+model User {
+  id            String   @id @default(auto()) @map("_id") @db.ObjectId
+  email         String   @unique
+  username      String
+  provider      String
+  hashedPassword String
+  coins         Int
+  avatarUrl     String
+  bio           String
+  verified      Boolean
+  conversations Conversation[]
+}
+
+model Verification {
+  email         String    @id @map("_id")
+  token         String         
+  expires       DateTime 
+}
+
+model Conversation {
+  id             String   @id @map("_id") @default(auto()) @db.ObjectId
+  contributor    User     @relation(fields: [contributorId], references: [id])
+  contributorId  String   @db.ObjectId
+  details  ConversationDetail[]
+}
+
+model ConversationDetail {
+  id         String   @id @map("_id") @default(auto()) @db.ObjectId
+  prompt     String
+  completions Completion[]
+  feedback   String?
+  conversation Conversation @relation(fields: [conversationId], references: [id])
+  conversationId String @db.ObjectId
+}
+
+model Completion {
+  id         String   @id @map("_id") @default(auto()) @db.ObjectId
+  content    String
+  model_name String
+  rating     Int
+  conversationDetail ConversationDetail @relation(fields: [conversationDetailId], references: [id])
+  conversationDetailId String @db.ObjectId
+}
+*/
+
+
+const db = new PrismaClient();
 
 export async function POST(request: NextRequest) {
   try {
+
     // Get the message from the request query
     const requestBody = await request.json();
     // Please use a validator in production
     const messages = requestBody.messages as Message[];
     const model = requestBody.model as string;
+    //ConversationId might be null, if it is, create a new conversation
+    const conversationId = requestBody.conversationId?.toString();
 
-    // Send the message to OpenAI
-    const response = await openai.chat.completions.create({
-      messages: messages,
-      model: model, //"gpt-3.5-turbo",
-      temperature: 0,
-      max_tokens: 1000,
-      // Set stream to true
-      stream: true,
+    const stream = await getStream(model, messages, async (response) => {
+      // Save the response to the database
     });
 
-    const stream = OpenAIStream(response, {
-      onCompletion: async (response) => {
-        // Do something with the response
-        console.log(response);
-      },
-    });
-    return new StreamingTextResponse(stream, {
+    return new StreamingTextResponse(stream || new ReadableStream(), {
       status: 200,
     });
   } catch (error) {
