ae868b4655b8ea71a5c0990c165a989908dc12f1
quevenliu <108058646+quevenliu@users.noreply.github.com>
2024-03-29 16:35:09 +0800

    refactor: commitizen and env update

--- src/lib/env/private.ts
+++ src/lib/env/private.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 const privateEnvSchema = z.object({
   DATABASE_URL: z.string(),
   OPENAI_KEY: z.string(),
+  ANTHROPIC_KEY: z.string(),
+  MISTRAL_KEY: z.string(),
+  RESEND_API_KEY: z.string(),
 });
 
 type PrivateEnv = z.infer<typeof privateEnvSchema>;
@@ -10,6 +13,9 @@ type PrivateEnv = z.infer<typeof privateEnvSchema>;
 export const privateEnv: PrivateEnv = {
   DATABASE_URL: process.env.DATABASE_URL!,
   OPENAI_KEY: process.env.OPENAI_KEY!,
+  ANTHROPIC_KEY: process.env.ANTHROPIC_KEY!,
+  MISTRAL_KEY: process.env.MISTRAL_KEY!,
+  RESEND_API_KEY: process.env.RESEND_API_KEY!,
 };
 
 privateEnvSchema.parse(privateEnv);
