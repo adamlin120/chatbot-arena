779f0fb44ff44461a5e5f3e7dcee92a4c4cdb162
biao <b10902133@ntu.edu.tw>
2024-03-30 23:02:00 +0800

    fix: fix the error occurs when deploying the app, and refactor something

--- src/app/_components/Loading.tsx
+++ src/app/_components/Loading.tsx
@@ -1,4 +1,4 @@
-export default function LoadingScreen() {
+export default function Loading() {
     return (
       <div className="flex w-full h-full pt-16 justify-center items-center">
         <div className="loading-spinner h-[100px] w-[100px]"></div>
