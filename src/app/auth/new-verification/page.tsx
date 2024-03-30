55058dcf2c0447fa4ad57308a4a716584dfca7c8
biao <b10902133@ntu.edu.tw>
2024-03-30 23:24:42 +0800

    fix(auth/new-verification): add Suspense to prevent the page to be blank until the client-side JavaScript has loaded
    As here: https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- src/app/auth/new-verification/page.tsx
+++ src/app/auth/new-verification/page.tsx
@@ -1,7 +1,11 @@
+import Loading from "@/app/_components/Loading";
 import { NewVerificationForm } from "@/app/_components/NewVerificationForm";
+import { Suspense } from "react";
 const NewVerificationPage = async() => {
     return (
-        <NewVerificationForm/>
+        <Suspense fallback={<Loading />} >
+            <NewVerificationForm/>
+        </Suspense>
     );
 }
 
