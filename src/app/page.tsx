db7cdb3c48e05c78dcac9bc3a6032d06b0e12e15
nukliliang <b10902112@ntu.edu.tw>
2024-04-02 04:03:36 +0800

    fix: remove credential, add social

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
-  if (session?.user?.email) {
+  if (session?.user?.name) {
     redirect("/rating");
   }
   return (
