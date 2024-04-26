b78bcf60caaf710f4c13ed3a4d881ca1cda1a41b
biao <b10902133@ntu.edu.tw>
2024-04-26 18:39:25 +0800

    fix: fix sidebar alignment issue, and add headbar subtitle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import SideBar from "./_components/SideBar";
 import Link from "next/link";
-// import { headers } from "next/headers";
+import HeaderSubtitle from "./_components/HeaderSubtitle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,34 +19,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const headerList = headers();
-  // const pathNames = headerList.get("referer");
-  // const currentPath = pathNames?.split("/")[3];
-  // const subtitle: { [key: string]: string } = {
-  //   chat: "語言模型競技場 ⚔️",
-  //   rating: "模型評分區 👍👎",
-  //   dataset: "對話資料集 📚",
-  //   leaderboard: "模型排行榜 🏆",
-  //   profile: "個人頁面",
-  // };
 
   return (
     <html lang="en">
       <SessionProvider>
         <body className={inter.className + "flex flex-col"}>
           {/* <Header /> */}
-
           <div className="flex flex-col md:min-h-screen flex-grow">
             <Link
               href="/"
-              className="text-2xl ml-[4rem] mt-6 font-semibold text-nowrap"
+              className="text-2xl ml-[4rem] mt-6 font-semibold text-nowrap w-fit"
             >
               LLM Arena
-              {/* <span className="text-xl">
-                {currentPath && subtitle[currentPath]
-                  ? ` - ${subtitle[currentPath]}`
-                  : ""}
-              </span> */}
+              <HeaderSubtitle />
             </Link>
             <div className="md:h-full mt-5 mb-16 md:mb-0 md:ml-16">
               {children}
