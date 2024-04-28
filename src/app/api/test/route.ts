c21612c392355bb8aa2ab842285cd7fa3321e7df
kevlius <108058646+quevenliu@users.noreply.github.com>
2024-04-28 14:47:42 +0800

    fix: edit content of test route

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,34 +2,6 @@
 
 import { NextRequest, NextResponse } from "next/server";
 
-import { db } from "../_base";
-
 export async function POST(req: NextRequest, res: NextResponse) {
-  const reqBody = await req.json();
-  const { email, username, provider, hashedPassword, coins, verified } =
-    reqBody;
-
-  try {
-    const user = await db.user.create({
-      data: {
-        email,
-        username,
-        provider,
-        hashedPassword,
-        coins,
-        verified,
-      },
-    });
-
-    return NextResponse.json(
-      { message: "Created Successfully!" },
-      { status: 201 },
-    );
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Something went wrong!" },
-      { status: 500 },
-    );
-  }
+  return NextResponse.json({ message: "GPTs Together Strong" });
 }
