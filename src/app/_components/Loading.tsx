991618c1a71c7e3fd76bb80dadb69026abc8711f
biao <b10902133@ntu.edu.tw>
2024-07-01 18:24:48 +0800

    refactor: change loading component to mui's

--- src/app/_components/Loading.tsx
+++ src/app/_components/Loading.tsx
@@ -1,7 +1,9 @@
+import { CircularProgress, CircularProgressProps } from "@mui/material";
 export default function Loading() {
   return (
     <div className="flex w-full h-full pt-16 justify-center items-center">
-      <div className="loading-spinner h-[100px] w-[100px]"></div>
+      {/* <div className="loading-spinner h-[100px] w-[100px]"></div> */}
+      <CircularProgress size={100} />
     </div>
   );
 }
