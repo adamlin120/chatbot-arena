ff5ee8ae764e3b6c54a0c05f83ee4981374a8f48
biao <b10902133@ntu.edu.tw>
2024-07-01 18:27:01 +0800

    fix: remove unused variable

--- src/app/_components/Loading.tsx
+++ src/app/_components/Loading.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress, CircularProgressProps } from "@mui/material";
+import { CircularProgress } from "@mui/material";
 export default function Loading() {
   return (
     <div className="flex w-full h-full pt-16 justify-center items-center">
