41bfce521d56f9ff16d03edfa714fc5ef3606644
Kuan-Fu Kevin Liu <108058646+quevenliu@users.noreply.github.com>
2024-10-24 16:21:53 +0800

    fix: user permanent redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
-
 export default async function Home() {
-  redirect("/chat");
+  redirect("/chat", {
+    permanent: true, // This sets the status code to 301
+  });
 }
